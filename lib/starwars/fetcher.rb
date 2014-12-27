fcdf884f38432b3cb777c82f3cf0ec2dd8124ad2
Moski Doski <abushaikh@gmail.com>
2014-12-27 20:03:04 +0200

    cleaning up the fetcher class

--- lib/starwars/fetcher.rb
+++ lib/starwars/fetcher.rb
@@ -11,7 +11,10 @@ module Starwars
     # Fetcher Methods to be included in every resource
     module InstanceMethods
       #
-      # Fetch a resouce give the resource name
+      # Fetch the resource from the remote URL and serialize it.
+      # It will delgate to the super function defined in base.rb
+      #
+      # @return [Starwars::Planet|Starwars::Film|Starwars::Person|Starwars::Specie|Starwars::Starship|Starwars::Vehicle]
       #
       def fetch
         super(resouces_name)
@@ -19,21 +22,36 @@ module Starwars
 
       private
 
+      #
+      # Return the resource name based on the including class.
+      #
+      # @return [String] the plural name of the resource.
+      #
       def resouces_name
-        resource = self.class.name.split('::').last.downcase
-        (resource == 'person') ? 'people' : "#{resource}s"
+        self.class::RESOURCE_NAME
       end
     end
 
     # Fetcher Class Method to be included in every resource
     module ClassMethods
+      #
+      # Fetch the resource from the remote URL and serialize it.
+      # It will delgate to the super function defined in base.rb
+      # @param id [Integer] the resource id
+      #
+      # @return [Starwars::Planet|Starwars::Film|Starwars::Person|Starwars::Specie|Starwars::Starship|Starwars::Vehicle]
+      #
       def fetch(id)
         super(resouces_name, id)
       end
 
+      #
+      # Return the resource name based on the including class.
+      #
+      # @return [String] the plural name of the resource.
+      #
       def resouces_name
-        resource = name.split('::').last.downcase
-        (resource == 'person') ? 'people' : "#{resource}s"
+        const_get('RESOURCE_NAME')
       end
     end
   end
